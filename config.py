f3698aaa4b9e777284fb709cb5c4753587599f17
AbdullinaLR <owlwithcookie@gmail.com>
2024-05-15 00:23:18 +0300

    попытка баз данных

--- config.py
+++ config.py
@@ -1,28 +1,3 @@
-# import os
-#
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///characters.db'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#
-# class DevelopmentConfig(Config):
-#     DEBUG = True
-#
-# class TestingConfig(Config):
-#     TESTING = True
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-#
-# class ProductionConfig(Config):
-#     DEBUG = False
-#     TESTING = False
-#     # Other production-specific settings
-#
-# config = {
-#     'development': DevelopmentConfig,
-#     'testing': TestingConfig,
-#     'production': ProductionConfig,
-#     'default': DevelopmentConfig
-# }
 import os
 
 
--- forms.py
+++ forms.py
@@ -1,14 +1,3 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, IntegerField, SubmitField
-# from wtforms.validators import DataRequired
-#
-#
-# class CharacterForm(FlaskForm):
-#     name = StringField('Name', validators=[DataRequired()])
-#     strength = IntegerField('Strength', validators=[DataRequired()])
-#     agility = IntegerField('Agility', validators=[DataRequired()])
-#     submit = SubmitField('Create Character')
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
