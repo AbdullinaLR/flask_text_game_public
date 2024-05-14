e96825436485bf6ec786704bfeed2ac83f3fe2f4
AbdullinaLR <owlwithcookie@gmail.com>
2024-05-14 23:00:58 +0300

    доп файлы

--- forms.py
+++ forms.py
@@ -1,10 +1,20 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, IntegerField, SubmitField
+# from wtforms.validators import DataRequired
+#
+#
+# class CharacterForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired()])
+#     strength = IntegerField('Strength', validators=[DataRequired()])
+#     agility = IntegerField('Agility', validators=[DataRequired()])
+#     submit = SubmitField('Create Character')
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
 
 
 class CharacterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    strength = IntegerField('Strength', validators=[DataRequired()])
-    agility = IntegerField('Agility', validators=[DataRequired()])
-    submit = SubmitField('Create Character')
+    strength = IntegerField('Strength', validators=[DataRequired(), NumberRange(min=1, max=100)])
+    agility = IntegerField('Agility', validators=[DataRequired(), NumberRange(min=1, max=100)])
--- models.py
+++ models.py
@@ -1,9 +1,25 @@
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy()
+#
+#
+# class Character(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     strength = db.Column(db.Integer, nullable=False)
+#     agility = db.Column(db.Integer, nullable=False)
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     strength = db.Column(db.Integer, nullable=False)
     agility = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Character {self.name}>'
+
